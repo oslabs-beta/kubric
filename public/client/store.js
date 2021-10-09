f4eb812192078a1112dce59365b6bbeeaa1607d1
laurabotel <laura.e.botel@gmail.com>
2021-10-09 11:26:24 -0400

    configured redux store

--- public/client/store.js
+++ public/client/store.js
@@ -1,4 +1,17 @@
 import { combineReducers, applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 
-// ADD ANY NEW REDUCERS TO rootReducer
+
+const rootReducer = combineReducers({
+  
+  // ADD ANY NEW REDUCERS HERE
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
