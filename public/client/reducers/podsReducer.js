8db54e65c4f3833db1c66952be02b5c5d2633308
laurabotel <laura.e.botel@gmail.com>
2021-10-09 11:43:29 -0400

    basic framework for podsReducer, case needs logic, need to choose a date library instead of moment, maybe date-fns?

---
+++ public/client/reducers/podsReducer.js
@@ -0,0 +1,21 @@
+import * as actionTypes from '../actions/actionTypes.js';
+// TODO: import a library here to formate date/time from metadata creationTime)
+
+const initialState = {
+  pods: [],
+}
+
+function podsReducer(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case actionTypes.RECEIVE_PODS:
+      let pods = [];
+    
+      return {...state, pods};
+    default: 
+      return state;
+  }
+
+
+}
