ecc5b7b224b520178bafec7393014de592ae71c0
laurabotel <laura.e.botel@gmail.com>
2021-10-10 16:08:28 -0400

    adding additional files for updates

--- public/client/reducers/metricsReducer.js
+++ public/client/reducers/metricsReducer.js
@@ -9,6 +9,16 @@ function metricsReducer (state = initialState, action) {
 
   switch (type) {
     // list and configure cases
+    case actionTypes.DEFAULT_METRICS_RECEIVED:
+      let { metrics } = payload;
+      let charts = [];
+
+      metrics.forEach(metric => {
+        console.log(metric);
+      })
+
+      // UPDATE THIS 
+      return state;
     default:
       return state;
   }
