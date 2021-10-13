ea8f442b7e40b3e23c67876e47b95c785909b2d0
jlhline <jlhline@gmail.com>
2021-10-12 23:20:55 -0400

    logs routing with new elasticsearch package

--- server/controllers/logsController.js
+++ server/controllers/logsController.js
@@ -1,8 +1,24 @@
 //will require database model for logs
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://localhost:9200' })
+
 const logsController = {};
 
-logsController.getLogs/*or whatever u want to name it*/ = (req, res, next) =>{
-
+logsController.getElasticLogs = async (req, res, next) => {
+    console.log("hit elastic")
+    
+    client.search({
+        index: 'total-logs',
+        size: 100,
+        body: { query: {
+            match: { 'type': 'server' }
+           }}
+      },(err, result) => {
+        if (err) console.log(err)
+        else {
+            res.locals.elastic = result.body.hits.hits;
+            next();
+        }
+      })
 }
-
 module.exports = logsController;
--- server/routes/logsRouter.js
+++ server/routes/logsRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
-const logsController = require('../controllers/logsController');
+const {getElasticLogs} = require('../controllers/logsController');
 const logsRouter = Router();
 
-//logsRouter.get()
+logsRouter.get('/',getElasticLogs,(req,res)=>{
+    console.log(res.locals.elastic)
+    res.status(200).json({elastic:res.locals.elastic})
+})
 
 module.exports = logsRouter;
