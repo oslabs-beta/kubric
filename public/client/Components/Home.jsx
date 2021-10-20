6b6bef2135a9e6bb15fa073c37d90073ced066b3
jlhline <jlhline@gmail.com>
2021-10-19 20:32:00 -0400

    dropdown menu

---
+++ public/client/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Kubric Home Page
+    </div>
+  )
+}
+export default Home;
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,11 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import App from '../public/client/App.jsx';
 import store from '../public/client/store.js'
-
+import ReactDOM from "react-dom";
 // import styles from scss/css so webpack can bundle styles
 import styles from '../public/client/scss/application.scss';
 
-render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
